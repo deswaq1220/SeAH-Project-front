aff7466c7a2801b3939bb6b7de34c2e5b9f9c30a
deswaq789 <deswaq789@naver.com>
2023-07-07 17:45:23 +0900

    메인수정중

--- src/page/Main.js
+++ src/page/Main.js
@@ -1,70 +1,28 @@
-import "../style/Main.css"
-import safety from "../img/safety.png";
-import worker from "../img/worker2.png";
-import mark from "../img/x-mark.png"
-import logo from "../img/logo.png"
+import "../style/Main.css";
 
 function Main() {
-
-  const products = [
-    {
-      id: 1,
-      name: '안전교육',
-      href: '#',
-      imageSrc: safety,
-      imageAlt: '안전교육',
-    },
-    {
-      id: 2,
-      name: '안전점검',
-      href: '#',
-      imageSrc: worker,
-      imageAlt: '안전점검',
-    },
-    {
-      id: 3,
-      name: '임시',
-      href: '#',
-      imageSrc: mark,
-      imageAlt: '임시',
-    },
-    {
-      id: 4,
-      name: '임시',
-      href: '#',
-      imageSrc: mark,
-      imageAlt: '임시',
-    },
-    
-    // More products...
-  ]
-
-
   return (
+    <div>
       <div id="wrap2">
-        <div className="bg-white mt-8" >
-        <img src={logo} className="mx-auto w-96" />
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {products.map((product) => (
-            <a key={product.id} href={product.href} className="group">
-              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg xl:aspect-h-8 xl:aspect-w-7 shadow">
-                <img
-                  src={product.imageSrc}
-                  alt={product.imageAlt}
-                  className="h-full w-full m-0 object-cover object-center group-hover:opacity-75"
-                />
-              </div>
-              <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
-              
-            </a>
-          ))}
+        <div id="menuWrap">
+          <div id="logo2"></div>
+        <div className="slideImg"></div>
+          <ul className="subWrap1">
+            <li>
+              <p>안전점검</p>
+            </li>
+            <li></li>
+            <li></li>
+          </ul>
+          <ul className="subWrap2">
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
         </div>
       </div>
     </div>
-
-      </div>
-  )
+  );
 }
 
 export default Main;
