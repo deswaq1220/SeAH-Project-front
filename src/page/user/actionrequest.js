d12e5303b1e85b3fbb8dafadda6b2707712c7e92
gesalburger111 <ly707278@gmail.com>
2023-08-03 20:53:41 +0900

    23090803[수시점검] post수정중
    값 세팅 다시 확인해야됨

--- src/page/user/actionrequest.js
+++ src/page/user/actionrequest.js
@@ -2,45 +2,44 @@ import { Fragment, useEffect, useState } from "react";
 import { Listbox, Transition } from "@headlessui/react";
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
 import axios from "axios";
-import {useParams} from "react-router-dom";
-
+import { useParams } from "react-router-dom";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-
-export default function ActionRquest({onFormDataChange}){
+export default function ActionRquest({ onFormDataChange }) {
   // get 정보
   // const [peopleSelected, SetPeopleSelected] = useState(people[0]); //  실수함정
   // qr 정보따라 url 파라미터 세팅되어야됨
-  const { masterdataPart } = useParams();           // url 영역 파라미터
-  const { masterdataFacility } = useParams();       // url 설비 파라미터
+  const { masterdataPart } = useParams(); // url 영역 파라미터
+  const { masterdataFacility } = useParams(); // url 설비 파라미터
   const [emailList, setEmailList] = useState([]);
 
   useEffect(() => {
-      function emailFetchDataWithAxios(masterdataPart, masterdataFacility){
-          axios
-              .get(`http://localhost:8081/special/new/${masterdataPart}/${masterdataFacility}`)
-              .then((response) => {
-                  const emailListFromBack = response.data.emailList;
+    function emailFetchDataWithAxios(masterdataPart, masterdataFacility) {
+      axios
+        .get(
+          `http://localhost:8081/special/new/${masterdataPart}/${masterdataFacility}`
+        )
+        .then((response) => {
+          const emailListFromBack = response.data.emailList;
 
-                  const emailData = emailListFromBack.map((item) =>{
-                      return {
-                          emailId : item.emailId,
-                          emailName : item.emailName,
-                          emailAdd : item.emailAdd,
-                          masterStatus : item.masterStatus
-                      };
-                  });
-                  setEmailList(emailData);
-              })
-              .catch((error) => {
-                  console.error("Error fetching data: ", error);
-              });
-      }
-      emailFetchDataWithAxios(masterdataPart, masterdataFacility);
-
+          const emailData = emailListFromBack.map((item) => {
+            return {
+              emailId: item.emailId,
+              emailName: item.emailName,
+              emailAdd: item.emailAdd,
+              masterStatus: item.masterStatus,
+            };
+          });
+          setEmailList(emailData);
+        })
+        .catch((error) => {
+          console.error("Error fetching data: ", error);
+        });
+    }
+    emailFetchDataWithAxios(masterdataPart, masterdataFacility);
   }, [masterdataPart, masterdataFacility]);
 
   // 선택된 이메일 담을 변수
@@ -48,118 +47,143 @@ export default function ActionRquest({onFormDataChange}){
 
   // post 정보
   const handleActionChange = (selectedEmail) => {
-      setSelectedEmail(selectedEmail);
-      onFormDataChange(selectedEmail)({
-          setActPerson : selectedEmail?.emailName || "",      // 선택한 이메일이 있다면 이름 저장
-          speActEmail: selectedEmail?.emailAdd || "",   // 선택한 이메일이 있다면 이메일 저장
-      });
+    setSelectedEmail(selectedEmail);
+    onFormDataChange({
+      speActPerson: selectedEmail?.emailName || "", // 선택한 이메일이 있다면 이름 저장
+      speActEmail: selectedEmail?.emailAdd || "", // 선택한 이메일이 있다면 이메일 저장
+    });
   };
 
+  const [instances, setInstances] = useState([{}]);
+  // 인스턴스 추가 함수
+  const handleAddInstance = () => {
+    setInstances([...instances, {}]);
+  };
+  const handleDeleteInstance = (index) => {
+    setInstances(instances.filter((_, i) => i !== index));
+  };
 
-
-
-
-
-  return(
-    <div id="ActionRequest" className="grid sm:flex items-baseline justify-start">
-        <span className=" w-20 inline-flex items-center justify-center rounded-md bg-red-50 px-3 py-1 text-sm font-medium text-seahColor ring-1 ring-inset ring-red-600/10 flex-grow-0 my-4 ml-4 ">
-          조치요청
-        </span>
+  return (
+    <div
+      id="ActionRequest"
+      className="grid sm:flex items-baseline justify-start"
+    >
+      <span className=" w-20 inline-flex items-center justify-center rounded-md bg-red-50 px-3 py-1 text-sm font-medium text-seahColor ring-1 ring-inset ring-red-600/10 flex-grow-0 my-4 ml-4 ">
+        조치요청
+      </span>
+      <div className="">
         {/* 조치요청 */}
+        {instances.map((instance, index) => (
+          <div key={index} className="flex justify-between">
+            {/*<Listbox value={selectedEmail} onChange={setSelectedEmail}>*/}
+            <Listbox value={selectedEmail} onChange={handleActionChange}>
+              {({ open }) => (
+                <>
+                  <div className="relative mt-2 ml-4">
+                    <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-32 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-seahColor sm:text-sm sm:leading-6 ml-2">
+                      <span className="block truncate">
+                        {selectedEmail ? selectedEmail.emailName : "[선택]"}
+                        {/* {peopleSelected.name} */}
+                      </span>
+                      <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                        <ChevronUpDownIcon
+                          className="h-5 w-5 text-gray-400"
+                          aria-hidden="true"
+                        />
+                      </span>
+                    </Listbox.Button>
 
-        <div className="grid sm:columns-1 ml-4">
-        {/*<Listbox value={selectedEmail} onChange={setSelectedEmail}>*/}
-        <Listbox value={selectedEmail} onChange={handleActionChange}>
-          {({ open }) => (
-            <>
-              <div className="relative mt-2">
-                <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-32 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-seahColor sm:text-sm sm:leading-6">
-                  <span className="block truncate">
-                      {selectedEmail ? selectedEmail.emailName : "[선택]"}
-                  </span>
-                  <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                    <ChevronUpDownIcon
-                      className="h-5 w-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                  </span>
-                </Listbox.Button>
+                    <Transition
+                      show={open}
+                      as={Fragment}
+                      leave="transition ease-in duration-100"
+                      leaveFrom="opacity-100"
+                      leaveTo="opacity-0"
+                    >
+                      <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                        {emailList.map((emailList) => (
+                          <Listbox.Option
+                            key={emailList.emailId}
+                            className={({ active }) =>
+                              classNames(
+                                active
+                                  ? "bg-seahColor text-white"
+                                  : "text-gray-900",
+                                "relative cursor-default select-none py-2 pl-3 pr-9"
+                              )
+                            }
+                            value={emailList}
+                          >
+                            {({ selected, active }) => (
+                              <>
+                                <span
+                                  className={classNames(
+                                    selected ? "font-semibold" : "font-normal",
+                                    "block truncate"
+                                  )}
+                                >
+                                  {emailList.emailName}
+                                </span>
 
-                <Transition
-                  show={open}
-                  as={Fragment}
-                  leave="transition ease-in duration-100"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
-                >
-                  <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                    {emailList.map((emailList) => (
-                      <Listbox.Option
-                        key={emailList.emailId}
-                        className={({ active }) =>
-                          classNames(
-                            active
-                              ? "bg-seahColor text-white"
-                              : "text-gray-900",
-                            "relative cursor-default select-none py-2 pl-3 pr-9"
-                          )
-                        }
-                        value={emailList}
-                      >
+                                {selected ? (
+                                  <span
+                                    className={classNames(
+                                      active ? "text-white" : "text-seahColor",
+                                      "absolute inset-y-0 right-0 flex items-center pr-4"
+                                    )}
+                                  >
+                                    <CheckIcon
+                                      className="h-5 w-5"
+                                      aria-hidden="true"
+                                    />
+                                  </span>
+                                ) : null}
+                              </>
+                            )}
+                          </Listbox.Option>
+                        ))}
+                      </Listbox.Options>
+                    </Transition>
+                  </div>
+                </>
+              )}
+            </Listbox>
 
-                        {({ selected, active }) => (
-                          <>
-                            <span
-                              className={classNames(
-                                selected ? "font-semibold" : "font-normal",
-                                "block truncate"
-                              )}
-                            >
-                              {emailList.emailName}
-                            </span>
-
-                            {selected ? (
-                              <span
-                                className={classNames(
-                                  active ? "text-white" : "text-seahColor",
-                                  "absolute inset-y-0 right-0 flex items-center pr-4"
-                                )}
-                              >
-                                <CheckIcon
-                                  className="h-5 w-5"
-                                  aria-hidden="true"
-                                />
-                              </span>
-                            ) : null}
-                          </>
-                        )}
-                      </Listbox.Option>
-                    ))}
-                  </Listbox.Options>
-                </Transition>
-              </div>
-            </>
-          )}
-        </Listbox>
-
-        <div className="mt-2 ml-1">
-          <input
-            type="text"
-            name="Inspectionarea"
-            id="Inspectionarea"
-            placeholder="E-Mail"
-            autoComplete="off"
-            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-seahColor sm:text-sm sm:leading-6 px-1.5"
-            value={selectedEmail ? selectedEmail.emailAdd : ""}
-            readOnly
-            onChange={(e) => {
-                setSelectedEmail({
+            <div className="mt-2 ml-1 flex">
+              <input
+                type="text"
+                name="Inspectionarea"
+                id="Inspectionarea"
+                placeholder="E-Mail"
+                autoComplete=""
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-seahColor sm:text-sm sm:leading-6 px-1.5 ml-1"
+                value={selectedEmail ? selectedEmail.emailAdd : ""}
+                readOnly
+                onChange={(e) => {
+                  setSelectedEmail({
                     ...selectedEmail,
                     emailAdd: e.target.value,
-                });
-            }}
-          />
-        </div>
+                  });
+                }}
+              />
+              {index === 0 ? (
+                <button
+                  className="px-4 py-2 text-black border rounded-full hover:bg-seahColor hover:text-white ml-1"
+                  onClick={handleAddInstance}
+                >
+                  +
+                </button>
+              ) : (
+                <button
+                  className="px-4 py-2 text-black border rounded-full hover:bg-seahColor hover:text-white ml-1"
+                  onClick={() => handleDeleteInstance(index)}
+                >
+                  -
+                </button>
+              )}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/page/user/sourceDanger.js
+++ src/page/user/sourceDanger.js
@@ -2,15 +2,16 @@ import { Fragment, useState } from "react";
 import { Listbox, Transition } from "@headlessui/react";
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
 
-// 부상부위
-const injured = [
-  { id: 1, name: "[신체]" },
-  { id: 2, name: "[머리]" },
-  { id: 3, name: "[팔]" },
-  { id: 4, name: "[다리]" },
-  { id: 5, name: "[가슴]" },
-  { id: 6, name: "[등,허리]" },
-  { id: 7, name: "[안면]" },
+
+// 위험원인
+const source = [
+  { id: 1, name: "[선택]" },
+  { id: 2, name: "[설비원인]" },
+  { id: 3, name: "[작업방법]" },
+  { id: 4, name: "[점검불량]" },
+  { id: 5, name: "[정비불량]" },
+  { id: 6, name: "[지식부족]" },
+  { id: 7, name: "[불안전한 행동]" },
   { id: 8, name: "[기타(직접입력)]" },
 ];
 
@@ -18,36 +19,31 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-export default function Injured({onFormDataChange}) {
-  const [injuredSelected, setInjuredSelected] = useState(injured[0]); //  부상부위
-  const [customInjured, setCustomInjured] = useState("");
+export default function Dangersource({onFormDataChange}){
 
-  const handleInjuredChange = (injuredSelected) => {
-    setInjuredSelected(injuredSelected);
-    onFormDataChange(injuredSelected);
-  };
+const [sourceSelected, setSourceSelected] = useState(source[0]); // 위험분류
+const [customSource, setCustomSource] = useState("");
 
-  const handleSelectedChange = (value) => {
-    setInjuredSelected(value);
-    if (value.name === "[기타(직접입력)]") {
-      setCustomInjured(""); // Reset the custom danger input field
-    }
-  };
-
-  return (
-    <div id="injured" className="flex items-baseline justify-start">
-      <span className=" w-20 inline-flex items-center justify-center rounded-md bg-red-50 px-3 py-1 text-sm font-medium text-seahColor ring-1 ring-inset ring-red-600/10 flex-grow-0 m-4 ">
-        부상부위
-      </span>
-      <div className="flex flex-col">
-        {/* 부상부위 */}
-        {/*<Listbox value={injuredSelected} onChange={setInjuredSelected}>*/}
-        <Listbox value={injuredSelected} onChange={handleInjuredChange}>
+const handleSelectedChange = (sourceSelected) => {
+  setSourceSelected(sourceSelected);
+  if (sourceSelected.name === "[기타(직접입력)]") {
+    setCustomSource(""); // Reset the custom danger input field
+  }
+  onFormDataChange(sourceSelected);
+};
+  return(
+    <div id="danger" className="flex items-baseline justify-start">
+        <span className=" w-20 inline-flex items-center justify-center rounded-md bg-red-50 px-3 py-1 text-sm font-medium text-seahColor ring-1 ring-inset ring-red-600/10 flex-grow-0 m-4 ">
+          위험원인
+        </span>
+        <div className="flex flex-col">
+        {/*<Listbox value={sourceSelected} onChange={setSourceSelected}>*/}
+        <Listbox value={sourceSelected} onChange={handleSelectedChange}>
           {({ open }) => (
             <>
               <div className="relative mt-2">
                 <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-32 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-seahColor sm:text-sm sm:leading-6">
-                  <span className="block truncate">{injuredSelected.name}</span>
+                  <span className="block truncate">{sourceSelected.name}</span>
                   <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                     <ChevronUpDownIcon
                       className="h-5 w-5 text-gray-400"
@@ -64,7 +60,7 @@ export default function Injured({onFormDataChange}) {
                   leaveTo="opacity-0"
                 >
                   <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                    {injured.map((person) => (
+                    {source.map((person) => (
                       <Listbox.Option
                         key={person.id}
                         className={({ active }) =>
@@ -111,17 +107,16 @@ export default function Injured({onFormDataChange}) {
             </>
           )}
         </Listbox>
-        {/* Custom Input */}
-        {injuredSelected.name === "[기타(직접입력)]" && (
-          <input
-            type="text"
-            value={customInjured}
-            onChange={(e) => setCustomInjured(e.target.value)}
-            className="block w-40 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-seahColor sm:text-sm sm:leading-6 px-1.5 mt-1"
-            placeholder="직접 입력"
-          />
-        )}
+        {sourceSelected.name === "[기타(직접입력)]" && (
+        <input
+          type="text"
+          value={customSource}
+          onChange={(e) => setCustomSource(e.target.value)}
+          className="block w-40 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-seahColor sm:text-sm sm:leading-6 px-1.5 mt-1"
+          placeholder="직접 입력"
+        />
+      )}
+        </div>
       </div>
-    </div>
-  );
+  )
 }
