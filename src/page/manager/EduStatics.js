ad2a79455b688fbd19b6e464061e58a462352cce
수인 <l3esde67@naver.com>
2023-08-09 17:06:13 +0900

    주소변경 커밋

--- src/page/manager/EduStatics.js
+++ src/page/manager/EduStatics.js
@@ -106,8 +106,8 @@ function EduStatics() {
         const currentYear = getYear(currentDate);
         // 월&카테고리 별 교육시간 총 합계
         const response = await axios.get(
-          // `http://172.20.20.252:8081/edustatistics/getmonthlyedulist`, {   // 세아
-          `http://localhost:8081/edustatistics/getmonthlyedulist`, {
+           `http://172.20.20.252:8081/edustatistics/getmonthlyedulist`, {   // 세아
+          //`http://localhost:8081/edustatistics/getmonthlyedulist`, {
           params: {
             year: currentYear,
             month: currentMonth,
