acf73944dc41b1c2d51d3f2e4c785e6693523b29
gesalburger111 <ly707278@gmail.com>
2023-08-18 16:54:50 +0900

    230818 [수시점검] 상세페이지 이미지
    이미지 크기조절 필요

--- src/page/user/UserFrequentDetails/UserFrequentDetailsTable.js
+++ src/page/user/UserFrequentDetails/UserFrequentDetailsTable.js
@@ -10,26 +10,20 @@ export default function UserFrequentDetailsTable() {
   useEffect(() => {
     // API 요청을 보내 데이터를 가져옵니다.
     axios
-      .get(`${process.env.REACT_APP_API_BASE_URL}/special/detail/${speId}`) // 실제 API 주소로 수정해야합니다.
-      .then((response) => {
-        setInspectionData(response.data); // 데이터를 상태 변수에 저장합니다.
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
+        .get(`${process.env.REACT_APP_API_BASE_URL}/special/detail/${speId}`) // 실제 API 주소로 수정해야합니다.
+        .then((response) => {
+          setInspectionData(response.data); // 데이터를 상태 변수에 저장합니다.
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
   }, [speId]); // speId가 변경될 때마다 useEffect가 실행됩니다.
 
   if (!inspectionData) {
     // 데이터가 아직 로드되지 않았을 때의 처리
     return <div>Loading...</div>;
   }
-
-
-  // 호ㅘㄱ인용
-  console.log("확인용 inspectionData.speDetailFindId:"+inspectionData["speDetailFindId"]);
-  console.log("확인용 inspectionData.imageUrl:"+inspectionData["imageUrl"]);
-
 
   const formatDate = (dateString) => {
     const options = {
@@ -67,167 +61,165 @@ export default function UserFrequentDetailsTable() {
     }
   };
   return (
-    <div className="mx-auto max-w-7xl sm:px-6 lg:px-8 mt-5">
-      <div className="px-4 sm:px-0">
-        <h3 className="text-xl font-extrabold leading-7 text-gray-900">
-          수시점검
-        </h3>
-        <p className="mt-1 max-w-2xl text-base leading-6 text-gray-500">
-          등록한 해당 수시점검을 수정 및 조회가 가능합니다
-        </p>
-        <div className="flex justify-end mt-1">
+      <div className="mx-auto max-w-7xl sm:px-6 lg:px-8 mt-5">
+        <div className="px-4 sm:px-0">
+          <h3 className="text-xl font-extrabold leading-7 text-gray-900">
+            수시점검
+          </h3>
+          <p className="mt-1 max-w-2xl text-base leading-6 text-gray-500">
+            등록한 해당 수시점검을 수정 및 조회가 가능합니다
+          </p>
+          <div className="flex justify-end mt-1">
+            <button
+                type="button"
+                className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 mr-2"
+            >
+              삭제
+            </button>
+          </div>
+        </div>
+        <div className="mt-6 border-t border-gray-100">
+          <dl className="divide-y divide-gray-100">
+            <div className="bg-gray-50 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
+              <dt className="text-base font-bold leading-6 text-gray-900">
+                점검일시
+              </dt>
+              <dd className="mt-1 text-base leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                {formatDate(inspectionData.specialData.speDate)}
+              </dd>
+            </div>
+            <div className="bg-white px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
+              <dt className="text-base font-bold leading-6 text-gray-900">
+                점검자
+              </dt>
+              <dd className="mt-1 text-base leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                {inspectionData.specialData.spePerson}
+              </dd>
+            </div>
+            <div className="bg-gray-50 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
+              <dt className="text-base font-bold leading-6 text-gray-900">
+                점검영역
+              </dt>
+              <dd className="mt-1 text-base leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                {inspectionData.specialData.spePart}
+              </dd>
+            </div>
+            <div className="bg-white px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
+              <dt className="text-base font-bold leading-6 text-gray-900">
+                설비명
+              </dt>
+              <dd className="mt-1 text-base leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                {inspectionData.specialData.speFacility}
+              </dd>
+            </div>
+            <div className="bg-gray-50 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
+              <dt className="text-base font-bold leading-6 text-gray-900">
+                위험분류 및 부상부위
+              </dt>
+              <dd className="mt-1 text-base leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                {inspectionData.specialData.speDanger} /{" "}
+                {inspectionData.specialData.speInjure}
+              </dd>
+            </div>
+            <div className="bg-white px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
+              <dt className="text-base font-bold leading-6 text-gray-900">
+                위험원인
+              </dt>
+              <dd className="mt-1 text-base leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                {inspectionData.specialData.speCause}
+              </dd>
+            </div>
+            <div className="bg-gray-50 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
+              <dt className="text-base font-bold leading-6 text-gray-900">
+                실수함정
+              </dt>
+              <dd className="mt-1 text-base leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                {inspectionData.specialData.speTrap}
+              </dd>
+            </div>
+            <div className="bg-white px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
+              <dt className="text-base font-bold leading-6 text-gray-900">
+                위험성평가
+              </dt>
+              <dd className="mt-1 text-base leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                {getRiskAssessmentText(
+                    inspectionData.specialData.speRiskAssess
+                )}
+              </dd>
+            </div>
+            <div className="bg-gray-50 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
+              <dt className="text-base font-bold leading-6 text-gray-900">
+                완료요청기한
+              </dt>
+              <dd className="mt-1 text-base leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                {formatDate(inspectionData.specialData.speDeadline)}
+              </dd>
+            </div>
+            <div className="bg-white px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
+              <dt className="text-base font-bold leading-6 text-gray-900">
+                점검내용
+              </dt>
+              <dd className="mt-1 text-base leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                {inspectionData.specialData.speContent}
+              </dd>
+            </div>
+            <div className="bg-gray-50 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
+              <dt className="text-base font-bold leading-6 text-gray-900">
+                개선대책
+              </dt>
+              <dd className="mt-1 text-base leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                {inspectionData.specialData.speActContent}
+              </dd>
+            </div>
+            <div className="bg-white px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
+              <dt className="text-base font-bold leading-6 text-gray-900">
+                조치요청
+              </dt>
+              <dd className="mt-1 text-base leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                {inspectionData.specialData.speActPerson} /{" "}
+                {inspectionData.specialData.speActEmail}
+              </dd>
+            </div>
+            <div className="bg-gray-50 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
+              <dt className="text-base font-bold leading-6 text-gray-900">
+                완료여부
+              </dt>
+              <dd className="mt-1 text-base leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                {getComplateText(inspectionData.specialData.speComplete)}
+              </dd>
+            </div>
+            <div className="bg-white px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
+              <dt className="text-base font-bold leading-6 text-gray-900">
+                사진
+              </dt>
+              <dd className="mt-1 text-base leading-6 text-gray-700 sm:col-span-2 sm:mt-0 flex flex-wrap">
+                {inspectionData && inspectionData.imageUrls
+                    ? inspectionData.imageUrls.map((file, index) => (
+                        <img
+                            key={index}
+                            src={process.env.REACT_APP_API_BASE_URL + file}
+                            alt={`사진 ${index + 1}`}
+                        />
+                    ))
+                    : "첨부된 사진이 없습니다"}
+              </dd>
+            </div>
+          </dl>
+        </div>
+        <div className="flex justify-center my-4">
           <button
-            type="button"
-            className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 mr-2"
+              type="button"
+              className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 mr-2"
           >
-            삭제
+            확인
+          </button>
+          <button
+              type="button"
+              className="rounded-md bg-seahColor px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-seahDeep focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-seahColor"
+          >
+            수정 / 조치완료 등록
           </button>
         </div>
       </div>
-      <div className="mt-6 border-t border-gray-100">
-        <dl className="divide-y divide-gray-100">
-          <div className="bg-gray-50 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
-            <dt className="text-base font-bold leading-6 text-gray-900">
-              점검일시
-            </dt>
-            <dd className="mt-1 text-base leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-              {formatDate(inspectionData.speDetailFindId.speDate)}
-            </dd>
-          </div>
-          <div className="bg-white px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
-            <dt className="text-base font-bold leading-6 text-gray-900">
-              점검자
-            </dt>
-            <dd className="mt-1 text-base leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-              {inspectionData.speDetailFindId.spePerson}
-            </dd>
-          </div>
-          <div className="bg-gray-50 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
-            <dt className="text-base font-bold leading-6 text-gray-900">
-              점검영역
-            </dt>
-            <dd className="mt-1 text-base leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-              {inspectionData.speDetailFindId.spePart}
-            </dd>
-          </div>
-          <div className="bg-white px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
-            <dt className="text-base font-bold leading-6 text-gray-900">
-              설비명
-            </dt>
-            <dd className="mt-1 text-base leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-              {inspectionData.speDetailFindId.speFacility}
-            </dd>
-          </div>
-          <div className="bg-gray-50 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
-            <dt className="text-base font-bold leading-6 text-gray-900">
-              위험분류 및 부상부위
-            </dt>
-            <dd className="mt-1 text-base leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-              {inspectionData.speDetailFindId.speDanger} /{" "}
-              {inspectionData.speDetailFindId.speInjure}
-            </dd>
-          </div>
-          <div className="bg-white px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
-            <dt className="text-base font-bold leading-6 text-gray-900">
-              위험원인
-            </dt>
-            <dd className="mt-1 text-base leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-              {inspectionData.speDetailFindId.speCause}
-            </dd>
-          </div>
-          <div className="bg-gray-50 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
-            <dt className="text-base font-bold leading-6 text-gray-900">
-              실수함정
-            </dt>
-            <dd className="mt-1 text-base leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-              {inspectionData.speDetailFindId.speTrap}
-            </dd>
-          </div>
-          <div className="bg-white px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
-            <dt className="text-base font-bold leading-6 text-gray-900">
-              위험성평가
-            </dt>
-            <dd className="mt-1 text-base leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-              {getRiskAssessmentText(
-                inspectionData.speDetailFindId.speRiskAssess
-              )}
-            </dd>
-          </div>
-          <div className="bg-gray-50 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
-            <dt className="text-base font-bold leading-6 text-gray-900">
-              완료요청기한
-            </dt>
-            <dd className="mt-1 text-base leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-              {formatDate(inspectionData.speDetailFindId.speDeadline)}
-            </dd>
-          </div>
-          <div className="bg-white px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
-            <dt className="text-base font-bold leading-6 text-gray-900">
-              점검내용
-            </dt>
-            <dd className="mt-1 text-base leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-              {inspectionData.speDetailFindId.speContent}
-            </dd>
-          </div>
-          <div className="bg-gray-50 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
-            <dt className="text-base font-bold leading-6 text-gray-900">
-              개선대책
-            </dt>
-            <dd className="mt-1 text-base leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-              {inspectionData.speDetailFindId.speActContent}
-            </dd>
-          </div>
-          <div className="bg-white px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
-            <dt className="text-base font-bold leading-6 text-gray-900">
-              조치요청
-            </dt>
-            <dd className="mt-1 text-base leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-              {inspectionData.speDetailFindId.speActPerson} /{" "}
-              {inspectionData.speDetailFindId.speActEmail}
-            </dd>
-          </div>
-          <div className="bg-gray-50 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
-            <dt className="text-base font-bold leading-6 text-gray-900">
-              완료여부
-            </dt>
-            <dd className="mt-1 text-base leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-              {getComplateText(inspectionData.speDetailFindId.speComplete)}
-            </dd>
-          </div>
-          <div className="bg-white px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
-            <dt className="text-base font-bold leading-6 text-gray-900">
-              사진
-            </dt>
-            <dd className="mt-1 text-base leading-6 text-gray-700 sm:col-span-2 sm:mt-0 flex flex-wrap">
-              {inspectionData.urlList.length > 0 ? (
-                  inspectionData.urlList.map((imageUrl, index) => (
-                      <img
-                          key={index}
-                          src={`${process.env.REACT_APP_API_BASE_URL}/special/detail/images/${imageUrl}`}
-                          alt={`사진 ${index + 1}`}
-                      />
-                  ))
-              ) : (
-                  "첨부된 사진이 없습니다"
-              )}
-            </dd>
-          </div>
-        </dl>
-      </div>
-      <div className="flex justify-center my-4">
-        <button
-          type="button"
-          className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 mr-2"
-        >
-          확인
-        </button>
-        <button
-          type="button"
-          className="rounded-md bg-seahColor px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-seahDeep focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-seahColor"
-        >
-          수정 / 조치완료 등록
-        </button>
-      </div>
-    </div>
   );
 }
