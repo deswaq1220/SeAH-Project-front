68bb69f2980478d38e3830e54a437a656498be41
봉림동미스코리아 <deswaq789@naver.com>
2023-09-14 14:33:03 +0900

    약간의 수정

--- src/page/user/UserRegularInspectionStatus/InsRegularItem.js
+++ src/page/user/UserRegularInspectionStatus/InsRegularItem.js
@@ -54,7 +54,7 @@ export default function InsRegularItem() {
       {({ open }) => (
         <>
           <div className="relative mt-2">
-            <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-seahColor sm:text-sm sm:leading-6">
+            <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-32 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-seahColor sm:text-sm sm:leading-6">
             <span className="block truncate">
                       {selectedArea ? selectedArea.name : "선택"}
                     </span>
