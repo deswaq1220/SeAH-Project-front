8dc24abc32d50f7091eb5fcb0007d8f2939f2131
찌인 <119689104+mcektzja@users.noreply.github.com>
2023-09-14 12:46:15 +0900

    통계 엑셀저장 재수정/ 버튼 내용변

--- src/page/manager/SafetyRegularInspectionStatisticsMonth.js
+++ src/page/manager/SafetyRegularInspectionStatisticsMonth.js
@@ -508,7 +508,7 @@ function SafetyInspectionStatisticsMonthImg() {
                       className="rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-seahColor"
                       onClick={regularHandleExport}
                   >
-                    정기점검 엑셀 저장
+                    엑셀저장
                   </button>
                 </h1>
                 <div className="grid grid-cols-1 gap-6">
