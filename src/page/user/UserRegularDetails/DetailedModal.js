9290f8d4750998d22bbac2cdb816c292885ed7c4
HalroWorld <halro92@gmail.com>
2023-09-15 12:11:05 +0900

    수정한번더

--- src/page/user/UserRegularDetails/DetailedModal.js
+++ src/page/user/UserRegularDetails/DetailedModal.js
@@ -43,6 +43,7 @@ export default function DetailedModal({fetchData, actForm, index}) {
             regularActPerson: regularActPerson,
             regularActEmail: regularActEmail,
             // regularActContent: content.current.value,
+            regularActContent: fetchData.regularActContent,
             files: newFiles.length > 0 ? [...newFiles,...files] : [...files],
             index: index,
         });
@@ -280,7 +281,7 @@ export default function DetailedModal({fetchData, actForm, index}) {
                                         className="inline-flex w-full justify-center rounded-md bg-seahColor px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-seahDeep focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-seahColor sm:col-start-2"
                                         onClick={handleCancelClick}
                                     >
-                                        확인
+                                        닫기
                                     </button>
                                 )}
                             </div>
