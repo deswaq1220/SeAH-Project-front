a6888da69ab0ef9fc87bf882419a0f5e9ad1407a
봉림동미스코리아 <deswaq789@naver.com>
2023-09-26 14:00:55 +0900

    오픈소스 라이선스 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import {BrowserRouter as Router, Navigate, Route, Routes} from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
 import "./style/reset.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -15,12 +20,12 @@ import UserSelectInspection from "./page/user/UserSelectInspection";
 import Userfrequent from "./page/user/Userfrequent";
 import UserfrequentReg from "./page/user/UserfrequentReg";
 import UserfrequentCompleteReg from "./page/user/UserfrequentCompleteReg";
-import EduStatics from "./page/manager/EduStatics"
-import EduAttenStatics from "./page/manager/EduAttenStatics"
+import EduStatics from "./page/manager/EduStatics";
+import EduAttenStatics from "./page/manager/EduAttenStatics";
 import FrequentIns from "./page/manager/SafetyFrequentInspection";
 import AttenStatus from "./page/manager/AttendanceStatus";
 import UserFrequentDetails from "./page/user/UserFrequentDetails/UserFrequentDetails";
-import FrequentDetails from "./page/manager/FrequentDetails/FrequentDetails"
+import FrequentDetails from "./page/manager/FrequentDetails/FrequentDetails";
 import TrainingReport from "./components/TrainingReport";
 import SafetyManagerInspection from "./page/manager/SafetyManagerInspection";
 import RegularMain from "./page/user/RegularMain";
@@ -32,10 +37,12 @@ import SafetyInspectionStatisticsYear from "./page/manager/SafetyInspectionStati
 import SafetySpecialInspectionStatisticsMonth from "./page/manager/SafetySpecialInspectionStatisticsMonth";
 import SafetyRegularInspectionStatisticsMonth from "./page/manager/SafetyRegularInspectionStatisticsMonth";
 import UserRegularInspectionStatus from "./page/user/UserRegularInspectionStatus/UserRegularInspectionStatus";
-
+import Footer from "./components/Footer";
+import License from "./page/License";
 
 function App() {
-  console.log(`
+  console.log(
+    `
 
    %c███████╗ █████╗ ██╗   ██╗ ██████╗ 
    %c██╔════╝██╔══██╗██║   ██║██╔════╝ 
@@ -43,66 +50,181 @@ function App() {
    %c╚════██║██╔══██║╚██╗ ██╔╝██║   ██║
    %c███████║██║  ██║ ╚████╔╝ ╚██████╔╝
    %c╚══════╝╚═╝  ╚═╝  ╚═══╝   ╚═════╝ 
-  `,"color:#F84c0b","color:#f5652f","color:#fd8153","color:#fa936d","color:#fda787","color:#fda787",)
+  `,
+    "color:#F84c0b",
+    "color:#f5652f",
+    "color:#fd8153",
+    "color:#fa936d",
+    "color:#fda787",
+    "color:#fda787"
+  );
 
   const adminOnly = sessionStorage.getItem("adminOnly") === "adminOnly";
 
-
   return (
+    <>
       <Router>
         <Routes>
           {/* 관리자 */}
-          <Route path="/" element={<ManagerLogin/>}></Route> {/* 관리자 로그인페이지 */}
-          <Route path="/manager" element={<MainManager/>}></Route> {/* 관리자 메인페이지 */}
-          <Route path="/eduMain" element={<SafetyEducationMain/>}></Route>{/* 관리자 안전교육페이지 */}
-          <Route path="/edudetails/:eduId" element={<SafetyEduDetails />} />{/* 관리자 안전교육상세*/}
-          <Route path="/edureg" element={<SafetyEduReg />} />{/* 관리자 안전교육 등록페이지 */}
-          <Route path="/edureg/:eduId" element={<SafetyEduReg /> }/>{/* 관리자 안전교육 등록페이지 */}
-          <Route path="/managerinspection" element={adminOnly ? <SafetyManagerInspection /> : <Navigate to="/" replace />} />{/* 관리자 안점점검 페이지 */}
-          <Route path="/inspection" element={adminOnly ? <SafetyInspection /> : <Navigate to="/" replace />} />{/* 관리자 안점점검 페이지 */}
-          <Route path="/attenstatus/:eduId" element={<AttenStatus />} />{/* 관리자 출석현황 */}
-          <Route path="/training/:eduId" element={ <TrainingReport />} />{/* 관리자 출석현황 */}
-          <Route path="/reginfo" element={adminOnly ? <ReferenceInfo /> : <Navigate to="/" replace />} />{/* 관리자 기준정보페이지 */}
-          <Route path="/edustatistics" element={adminOnly ? <EduStatics /> : <Navigate to="/" replace />} />{/* 관리자 안전교육통계*/}
-          <Route path="/edustatistics/atten" element={adminOnly ? <EduAttenStatics /> : <Navigate to="/" replace />} />{/* 관리자 안전교육통계*/}
-          <Route path="/inspection/statistics/year" element={adminOnly ? <SafetyInspectionStatisticsYear /> : <Navigate to="/" replace />} />{/* 관리자 연간분석(정기점검/수시점검) 통계 대시보드*/}
-          <Route path="/inspection/statistics/month/special" element={adminOnly ? <SafetySpecialInspectionStatisticsMonth /> : <Navigate to="/" replace />} />{/* 관리자 월간분석(수시점검) 대시보드*/}
-          <Route path="/inspection/statistics/month/regular" element={adminOnly ? <SafetyRegularInspectionStatisticsMonth /> : <Navigate to="/" replace />} />{/* 관리자 월간분석(정기점검) 대시보드*/}
-
-        {/* 공통 */}
-        <Route path="/regular" element={<RegularMain/>}></Route>{/* 관리자,사용자 정기점검 목록 */}
-        <Route path="/regulardetails" element={<UserRegularDetails/>}></Route>{/* 관리자,사용자 정기점검 상세페이지 */}
-        <Route path="/frequentinspection" element={<FrequentIns/>}></Route>{/* 공통 수시점검 현황*/}
-        <Route path="/all/special/detail/:speId" element={<FrequentDetails/>}></Route>{/* 공통 수시점검 상세페이지 */}
-        <Route path="/regularinsstatus" element={<UserRegularInspectionStatus/>}></Route>{/* 정기점검 현황 */}
-
-        {/* 사용자  */}
-        <Route path="/userattendance/register/:eduId" element={<UserSafetyEduAttendance/>}></Route>{/* 사용자 사원출석페이지 */}
-
-
-        {/* 사용자 수시점검 */}
-        <Route path="/special/:masterdataPart/:masterdataId" element={<UserSelectInspection/>}></Route>  {/* 사용자 점검선택페이지*/}
-        <Route path="/special/list/:masterdataPart/:masterdataId" element={<Userfrequent/>}></Route>  {/* 사용자 수시점검 설비별 리스트페이지*/}
-        <Route path="/special/new/:masterdataPart/:masterdataId" element={<UserfrequentReg/>}></Route>{/* 사용자 수시점검등록 / 수정페이지 */}
-        <Route path="/special/complete/:masterdataPart/:masterdataId" element={<UserfrequentCompleteReg/>}></Route>{/* 사용자 수시점검 완료등록 페이지 */}
-
-        <Route path="/userfrequentreg" element={<UserfrequentReg/>}></Route>{/* 수시점검 등록페이지 */}
-        <Route path="/special/detail/:speId" element={<UserFrequentDetails/>}></Route>{/* 수시점검 상세페이지 */}
-
-        {/* 정기점검 */}
-        <Route path="/regular" element={<RegularMain/>}></Route>{/* 관리자,사용자 정기점검 목록 */}
-        <Route path="/regulardetails/:regularId" element={<UserRegularDetails/>}></Route>{/* 관리자,사용자 정기점검 목록 */}
-
-
-        {/* 사용자 정기점검 */}
-        <Route path="/regularreg" element={<UserRegularReg/>}></Route>{/* 정기점검 등록페이지 */}
-
-
-        <Route path="/404" element={<Error/>}></Route> {/* 에러 */}
-      </Routes>
-      <ToastContainer/>
-    </Router>
-    
+          <Route path="/" element={<ManagerLogin />}></Route>{" "}
+          {/* 관리자 로그인페이지 */}
+          <Route path="/manager" element={<MainManager />}></Route>{" "}
+          {/* 관리자 메인페이지 */}
+          <Route path="/eduMain" element={<SafetyEducationMain />}></Route>
+          {/* 관리자 안전교육페이지 */}
+          <Route path="/edudetails/:eduId" element={<SafetyEduDetails />} />
+          {/* 관리자 안전교육상세*/}
+          <Route path="/edureg" element={<SafetyEduReg />} />
+          {/* 관리자 안전교육 등록페이지 */}
+          <Route path="/edureg/:eduId" element={<SafetyEduReg />} />
+          {/* 관리자 안전교육 등록페이지 */}
+          <Route
+            path="/managerinspection"
+            element={
+              adminOnly ? (
+                <SafetyManagerInspection />
+              ) : (
+                <Navigate to="/" replace />
+              )
+            }
+          />
+          {/* 관리자 안점점검 페이지 */}
+          <Route
+            path="/inspection"
+            element={
+              adminOnly ? <SafetyInspection /> : <Navigate to="/" replace />
+            }
+          />
+          {/* 관리자 안점점검 페이지 */}
+          <Route path="/attenstatus/:eduId" element={<AttenStatus />} />
+          {/* 관리자 출석현황 */}
+          <Route path="/training/:eduId" element={<TrainingReport />} />
+          {/* 관리자 출석현황 */}
+          <Route
+            path="/reginfo"
+            element={
+              adminOnly ? <ReferenceInfo /> : <Navigate to="/" replace />
+            }
+          />
+          {/* 관리자 기준정보페이지 */}
+          <Route
+            path="/edustatistics"
+            element={adminOnly ? <EduStatics /> : <Navigate to="/" replace />}
+          />
+          {/* 관리자 안전교육통계*/}
+          <Route
+            path="/edustatistics/atten"
+            element={
+              adminOnly ? <EduAttenStatics /> : <Navigate to="/" replace />
+            }
+          />
+          {/* 관리자 안전교육통계*/}
+          <Route
+            path="/inspection/statistics/year"
+            element={
+              adminOnly ? (
+                <SafetyInspectionStatisticsYear />
+              ) : (
+                <Navigate to="/" replace />
+              )
+            }
+          />
+          {/* 관리자 연간분석(정기점검/수시점검) 통계 대시보드*/}
+          <Route
+            path="/inspection/statistics/month/special"
+            element={
+              adminOnly ? (
+                <SafetySpecialInspectionStatisticsMonth />
+              ) : (
+                <Navigate to="/" replace />
+              )
+            }
+          />
+          {/* 관리자 월간분석(수시점검) 대시보드*/}
+          <Route
+            path="/inspection/statistics/month/regular"
+            element={
+              adminOnly ? (
+                <SafetyRegularInspectionStatisticsMonth />
+              ) : (
+                <Navigate to="/" replace />
+              )
+            }
+          />
+          {/* 관리자 월간분석(정기점검) 대시보드*/}
+          {/* 공통 */}
+          <Route path="/regular" element={<RegularMain />}></Route>
+          {/* 관리자,사용자 정기점검 목록 */}
+          <Route
+            path="/regulardetails"
+            element={<UserRegularDetails />}
+          ></Route>
+          {/* 관리자,사용자 정기점검 상세페이지 */}
+          <Route path="/frequentinspection" element={<FrequentIns />}></Route>
+          {/* 공통 수시점검 현황*/}
+          <Route
+            path="/all/special/detail/:speId"
+            element={<FrequentDetails />}
+          ></Route>
+          {/* 공통 수시점검 상세페이지 */}
+          <Route
+            path="/regularinsstatus"
+            element={<UserRegularInspectionStatus />}
+          ></Route>
+          {/* 정기점검 현황 */}
+          {/* 사용자  */}
+          <Route
+            path="/userattendance/register/:eduId"
+            element={<UserSafetyEduAttendance />}
+          ></Route>
+          {/* 사용자 사원출석페이지 */}
+          {/* 사용자 수시점검 */}
+          <Route
+            path="/special/:masterdataPart/:masterdataId"
+            element={<UserSelectInspection />}
+          ></Route>{" "}
+          {/* 사용자 점검선택페이지*/}
+          <Route
+            path="/special/list/:masterdataPart/:masterdataId"
+            element={<Userfrequent />}
+          ></Route>{" "}
+          {/* 사용자 수시점검 설비별 리스트페이지*/}
+          <Route
+            path="/special/new/:masterdataPart/:masterdataId"
+            element={<UserfrequentReg />}
+          ></Route>
+          {/* 사용자 수시점검등록 / 수정페이지 */}
+          <Route
+            path="/special/complete/:masterdataPart/:masterdataId"
+            element={<UserfrequentCompleteReg />}
+          ></Route>
+          {/* 사용자 수시점검 완료등록 페이지 */}
+          <Route path="/userfrequentreg" element={<UserfrequentReg />}></Route>
+          {/* 수시점검 등록페이지 */}
+          <Route
+            path="/special/detail/:speId"
+            element={<UserFrequentDetails />}
+          ></Route>
+          {/* 수시점검 상세페이지 */}
+          {/* 정기점검 */}
+          <Route path="/regular" element={<RegularMain />}></Route>
+          {/* 관리자,사용자 정기점검 목록 */}
+          <Route
+            path="/regulardetails/:regularId"
+            element={<UserRegularDetails />}
+          ></Route>
+          {/* 관리자,사용자 정기점검 목록 */}
+          {/* 사용자 정기점검 */}
+          <Route path="/regularreg" element={<UserRegularReg />}></Route>
+          {/* 정기점검 등록페이지 */}
+          <Route path="/404" element={<Error />}></Route> {/* 에러 */}
+          <Route path="/license" element={<License/>}></Route>
+        </Routes>
+        <ToastContainer />
+        <Footer />
+        
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,27 @@
+
+import { Link } from "react-router-dom"
+
+export default function Footer() {
+  return (
+    <footer className="bg-white" aria-labelledby="footer-heading">
+      <h2 id="footer-heading" className="sr-only">
+        Footer
+      </h2>
+      <div className="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
+      
+        <div className="mt-8 border-t border-gray-900/10 pt-8 md:flex md:items-center md:justify-between">
+          <div className="flex space-x-6 md:order-2">
+            <Link to="/license">
+              <h3 className="text-sm  leading-6 text-gray-400">오픈소스 라이선스</h3>
+            </Link>
+            
+          </div>
+           
+          {/* <p className="mt-8 text-xs leading-5 text-gray-500 md:order-1 md:mt-0">
+            &copy; 2020 Your Company, Inc. All rights reserved.
+          </p> */}
+        </div>
+      </div>
+    </footer>
+  )
+}
